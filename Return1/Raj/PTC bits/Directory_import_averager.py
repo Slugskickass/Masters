e80dd5223be6d7fd4cb2578a571152271471bcff
RajSeehra <rsseehra1@sheffield.ac.uk>
2020-03-16 13:23:34 +0000

    Latest version

--- Return1/Raj/PTC bits/Directory_import_averager.py
+++ Return1/Raj/PTC bits/Directory_import_averager.py
@@ -2,10 +2,10 @@ import Samurai as sam
 import numpy as np
 import sys
 
-# folder = sys.argv[1]
-# directory = sam.get_file_list(folder)
+folder = sys.argv[1]
+directory = sam.get_file_list(folder)
 
-directory = sam.get_file_list("/Users/RajSeehra/University/Masters/Semester 2/")
+#directory = sam.get_file_list("/Users/RajSeehra/University/Masters/Semester 2/")
 # directory = sam.get_file_list("/ugproj/raj/Flash4/")
 
 means = []  # empty list which will collect the means data we want
