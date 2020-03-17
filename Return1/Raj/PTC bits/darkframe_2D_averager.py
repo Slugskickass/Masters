afd22da7a5d2ed446ea4e0e887cabdd50bca56c4
RajSeehra <rsseehra1@sheffield.ac.uk>
2020-03-17 11:39:04 +0000

    Latest version

--- Return1/Raj/PTC bits/darkframe_2D_averager.py
+++ Return1/Raj/PTC bits/darkframe_2D_averager.py
@@ -4,11 +4,13 @@ import PTC_Samurai as sam
 
 file = sam.loadtiffs("/Users/RajSeehra/University/Masters/Semester 2/Teaching_python-master/Week 2/Data/640.tif")
 
+
 def axisaverager(file):
     array = np.mean(file, 2)
-
     return array
 
+
 array = axisaverager(file)
+
 
 # sam.savetiffs("darkframes.npy", array)
