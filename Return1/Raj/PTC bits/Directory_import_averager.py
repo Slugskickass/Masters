bd0fec08edef2c0420954907d08920f02fa0711a
RajSeehra <rsseehra1@sheffield.ac.uk>
2020-03-17 11:34:53 +0000

    Latest version

--- Return1/Raj/PTC bits/Directory_import_averager.py
+++ Return1/Raj/PTC bits/Directory_import_averager.py
@@ -2,21 +2,17 @@ import PTC_Samurai as sam
 import numpy as np
 import sys
 
-folder = sys.argv[1]
-directory = sam.get_file_list(folder)
+# folder = sys.argv[1]
+# directory = sam.get_file_list(folder)
 
-#directory = sam.get_file_list("/Users/RajSeehra/University/Masters/Semester 2/")
+directory = sam.get_file_list("/Users/RajSeehra/University/Masters/Semester 2/")
 # directory = sam.get_file_list("/ugproj/raj/Flash4/")
 
 means = []  # empty list which will collect the means data we want
 # imports the data.
 for i in range (0, len(directory)):
-    mean = []       # empty list where means can be stored for each file.
     array = sam.loadtiffs(directory[i])
-    for frame in range(np.size(array, 2)+1):      # calculates the mean for every frame.
-        mean.append(np.mean(array[frame]))        # mean per frame
-    means.append(np.mean(mean))                          # cleans up the mean data and appends to our means list
-    mean = []
+    means.append(np.mean(array))  # mean for the file
 
 min_value_location = means.index(min(means))
 print("The file with the lowest average intensity is: " + directory[min_value_location])
