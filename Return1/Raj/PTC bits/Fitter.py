58f3db32383dc14679e302ad715c474959089d27
RajSeehra <rsseehra1@sheffield.ac.uk>
2020-03-19 16:22:34 +0000

    my data fitted

--- Return1/Raj/PTC bits/Fitter.py
+++ Return1/Raj/PTC bits/Fitter.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
-mean_std = np.load("/Users/RajSeehra/University/Masters/Semester 2/Git Upload Folder/Return1/Raj/PTC bits/mean_std_data.npy")
+mean_std = np.load("/Users/RajSeehra/University/Masters/Semester 2/Git Upload Folder/Return1/Raj/PTC bits/PTCdata.npy")
+mean_std = mean_std.T
 
 x_data = []
 std = []
@@ -18,7 +19,7 @@ slope, intercept, r_value, p_value, std_err = stats.linregress(x_data, y_data)
 
 print(slope, intercept)
 plt.plot(slope, intercept, "*")
-plt.plot(x_data, y_data)
+plt.scatter(x_data, y_data)
 plt.title("PTC (cut out mean <10 or >1000)")
 plt.text(50, 25, 'Slope= '+ str(slope) + ', Intercept= ' + str(intercept))
 plt.xlabel('Mean')
