60112b1f0ba8ee7be4097c91a012da77c3cc666e
RajSeehra <rsseehra1@sheffield.ac.uk>
2020-03-19 12:59:00 +0000

    per pixel version

--- Return1/Raj/PTC bits/Fitter.py
+++ Return1/Raj/PTC bits/Fitter.py
@@ -4,4 +4,23 @@ from scipy import stats
 
 mean_std = np.load("/Users/RajSeehra/University/Masters/Semester 2/Git Upload Folder/Return1/Raj/PTC bits/mean_std_data.npy")
 
-print(mean_std)
+x_data = []
+std = []
+
+for i in range(0,len(mean_std)):
+    if 10 < mean_std[i, 0] < 1000:
+        x_data.append(mean_std[i, 0])
+        std.append((mean_std[i, 1]))
+
+y_data = np.square(std)
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(x_data, y_data)
+
+print(slope, intercept)
+plt.plot(slope, intercept, "*")
+plt.plot(x_data, y_data)
+plt.title("PTC (cut out mean <10 or >1000)")
+plt.text(50, 25, 'Slope= '+ str(slope) + ', Intercept= ' + str(intercept))
+plt.xlabel('Mean')
+plt.ylabel('Standard Deviation Squared')
+plt.show()
