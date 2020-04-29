7d7512aba154bde4ec3704469897edc8c4d9026f
mattarnoldbio <mattarnold@Matts-MBP.home>
2020-04-29 16:18:10 +0100

    Wobbly code, pls help Ash

--- SIM_Week/Matt/psf_calculator.py
+++ SIM_Week/Matt/psf_calculator.py
@@ -14,6 +14,8 @@ from skimage.morphology import closing, square
 from skimage.measure import label, regionprops, regionprops_table
 from scipy.optimize import curve_fit
 import scipy as sp
+from matplotlib.colors import LogNorm
+from matplotlib import cm
 
 
 def centre_collection(thresholded_data, scale=1):
@@ -38,15 +40,15 @@ def point_crop (data, y, x, side_val):
     half_side = int(side_length/2)
 #    contingency for if square would exit frame area
     if y - side_length < 0: 
-        y = 0 + half_side + 1
+        y = 0 + half_side
     if y + side_length > data.shape[1]:
-        y = (data.shape[1])-half_side
+        y = (data.shape[1])-half_side -1
     if x - side_length < 0:
-        x = 0 + half_side + 1
+        x = 0 + half_side
     if x + side_length > data.shape[0]:
-        x = (data.shape[0])-half_side
+        x = (data.shape[0])-half_side -1
 #    frame crop performed
-    square_result = data[y - (half_side+1):y + half_side,x - (half_side+1):x + half_side]
+    square_result = data[y - half_side:y + half_side +1, x - half_side:x + half_side + 1]
     
 #    return the square crop area as an ndarray
     return square_result
@@ -55,9 +57,11 @@ def point_crop (data, y, x, side_val):
 def crop(beads_frame, mic_array):
     
     a = beads_frame.shape[0]
-
-    save_array = np.zeros((a,15,15))
+        
+    size = 20
     
+    save_array = np.zeros((a, mic_array.shape[1], mic_array.shape[1]))
+        
     
     for x in range(0,beads_frame.shape[0]):
         # Define crop area from centre of mass data in imported locations table    
@@ -65,25 +69,76 @@ def crop(beads_frame, mic_array):
         centre_x = beads_frame.iloc[x,2]
         
         # Call cropping function
-        bead_crop = point_crop(mic_array, centre_y, centre_x, 15)
+        bead_crop = point_crop(mic_array, centre_y, centre_x, size)
         
-        save_array[x,:,:] = bead_crop
+        save_array[x,mic_array.shape[1]//2 - size//2: mic_array.shape[1]//2 + size//2 +1,mic_array.shape[1]//2 - size//2: mic_array.shape[1]//2 + size//2 +1] = bead_crop
         
     return save_array
 
+def data_extract(file_path):
+    ref = funci.load_img(file_path)
+    sum_ref = np.sum(ref,2)
+    
+    beads = centre_collection(sum_ref)
+    beads_clean1 = beads[beads["area"] < 16]
+    beads_clean =  beads_clean1[beads_clean1["area"] > 2]
+    
+    crop_beads = crop(beads_clean, sum_ref)
 
-ref = funci.load_img("/Users/mattarnold/Masters/SIM_week/Data/SLM-SIM_Tetraspeck200_680nm.tif")
-sum_ref = np.sum(ref,2)
+    psf = np.mean(crop_beads, axis=0)
+    
+    mtf_init = sp.fft.fft2(psf)
+    
+    mtf = sp.fft.fftshift(mtf_init)
+    
+    return psf, mtf, sum_ref
 
-beads = centre_collection(sum_ref)
-beads_clean1 = beads[beads["area"] < 16]
-beads_clean =  beads_clean1[beads_clean1["area"] > 2]
 
-crop_beads = crop(beads_clean, sum_ref)
+def complex_conj_test(data_file_path, mtf):
+    data= funci.load_img(data_file_path)
+    print("c")
+    data_fft = sp.fft.fftshift(sp.fft.fft2(data[:,:,1]))
+    print("d")
+    mtf_convolve = sp.signal.convolve2d(mtf,data_fft)
+    print("e")
+#    complex_conj = np.conj(mtf_convolve)
+#    print("f")
+    return data_fft, data[:,:,1] #,complex_conj
 
-psf = np.mean(crop_beads, axis=0)
+print("a")
 
-mtf = sp.fft.fft2(psf)
+psf, mtf, sum_ref = data_extract("/Users/mattarnold/Masters/SIM_week/Data/SLM-SIM_Tetraspeck200_680nm.tif")
 
-plt.imshow(abs(mtf))
+print("b")
+
+data_fft , data = complex_conj_test("/Users/mattarnold/Masters/SIM_week/Data/TIRF_Tubulin_525nm.tif", mtf)
+
+
+figure, axes = plt.subplots(figsize = (12,16))
+plt.subplot(321)
+plt.imshow(sum_ref)
+plt.title("Summed reference image")
+
+plt.subplot(323)
+plt.imshow(psf)
+plt.title("PSF")
+
+plt.subplot(324)
+plt.imshow((np.abs(mtf)))
+plt.title("Calculated MTF")
 plt.show
+
+plt.subplot(325)
+plt.imshow(data)
+plt.title("Data")
+plt.show
+
+plt.subplot(326)
+plt.imshow((np.abs(data_fft)),norm=LogNorm(vmin=5))
+plt.title("Data FFT")
+plt.show
+
+#plt.subplot(326)
+#plt.imshow((np.abs(conj)))
+#plt.title("Comlex conjugate")
+#plt.show
