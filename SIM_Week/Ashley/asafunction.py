d74ba66ad739952cbbb57a18425dfecbc4223c0b
slugskickass <a.cadby@sheffield.ac.uk>
2020-05-04 11:16:12 +0100

    Added the reading and data for SIM week, a little like national geographics Shark week but for SIM

--- SIM_Week/Ashley/asafunction.py
+++ SIM_Week/Ashley/asafunction.py
@@ -1,5 +1,39 @@
 import numpy as np
 import scipy.fft as fft
+from PIL import Image
+
+def get_image(filename, frame):
+    image = Image.open(filename)
+    image.seek(frame)
+    data = np.asarray(image)
+    return(data)
+
+def generate_PSF(NA, lamda, pixel_size, frame_size):
+    # FWHM = 2.3548 * sigma
+    sigma = (2.3548 * lamda / (2 * NA)) / pixel_size
+    t = frame_size
+    xo = np.floor(t/2)
+    u = np.linspace(0, t-1, t)
+    v = np.linspace(0, t-1, t)
+    [U, V] = np.meshgrid(u, v)
+    snuggle = np.exp(-1 * ((((xo - U)**2)/sigma**2) + (((xo - V)**2)/sigma**2)))
+    return(snuggle)
+
+def return_shiffetd_fft(Image):
+    fft_im = fft.fft2(Image)
+    fft_im_sh = fft.fftshift(fft_im)
+    return(fft_im_sh)
+
+def generate_PSF(NA, lamda, pixel_size, frame_size):
+    # FWHM = 2.3548 * sigma
+    sigma = (2.3548 * lamda / (2 * NA)) / pixel_size
+    t = frame_size
+    xo = np.floor(t/2)
+    u = np.linspace(0, t-1, t)
+    v = np.linspace(0, t-1, t)
+    [U, V] = np.meshgrid(u, v)
+    snuggle = np.exp(-1 * ((((xo - U)**2)/sigma**2) + (((xo - V)**2)/sigma**2)))
+    return(snuggle)
 
 # Done ?
 def OTFedgeF(OTFo):
@@ -15,6 +49,7 @@ def OTFedgeF(OTFo):
         i = i + 1;
     return(Kotf)
 
+# Done ?
 def PhaseKai2opt(k2fa, fS1aTnoisy, OTFo):
 #% The size of the image
     w = np.shape(fS1aTnoisy)[0]
@@ -44,3 +79,14 @@ def PhaseKai2opt(k2fa, fS1aTnoisy, OTFo):
     mA = mA / np.sum(fS1aT0 * np.conj(fS1aT0))
 
     return(mA)
+
+
+if __name__ == '__main__':
+    filename = '/Users/Ashley/PycharmProjects/SIMple/Data/SLM-SIM_Tetraspeck200_680nm.tif'
+
+    original_data = get_image(filename, 1)
+    fS1aTnoisy = return_shiffetd_fft(original_data)
+    k2fa = [0.01, 114]
+    psf = generate_PSF(1.2, 680, 97, 512)
+    OTFo = return_shiffetd_fft(psf)
+
