f90f313c121b04b74c2dcba378f0c8e5b1d72d85
slugskickass <a.cadby@sheffield.ac.uk>
2020-05-06 12:00:47 +0100

    Added minimizatin

--- SIM_Week/Ashley/asafunction.py
+++ SIM_Week/Ashley/asafunction.py
@@ -1,6 +1,18 @@
 import numpy as np
 import scipy.fft as fft
 from PIL import Image
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+
+def generate_patter(size, K_vector, ):
+    t = size
+    u = np.linspace(0, t-1, t)
+    v = np.linspace(0, t-1, t)
+    [U, V] = np.meshgrid(u, v)
+    to = t/2
+    i = np.complex(0, 1)
+    S1aT = np.exp(-1*i * 2*np.pi*( K_vector[0]/t*(U-to)+K_vector[1]/t*(V-to)))
+    return(S1aT)
 
 ## Image loaded into programme and appended, frame-wise to a 3D numpy array
 def get_image(filename, frame):
@@ -27,20 +39,20 @@ def return_shiffetd_fft(Image):
     fft_im_sh = fft.fftshift(fft_im)
     return fft_im_sh
 
-
 # Done ?
     #Input: OTF of system (o denoting microscope system base).
     #Return: point in fourier soace which marks the edge of the OTF
 def OTFedgeF(OTFo):
     w = np.shape(OTFo)[0] #Shape of OTF in a single axis (returns side length), then find half of this
-    wo = w/2        # Should this be w//2 as it is needs to be an integer. Maybe a plus one if required too.
+    wo = np.int(np.floor((w/2)))       # Should this be w//2 as it is needs to be an integer. Maybe a plus one if required too.
 
     # Makes a 1D array.
     OTF1 = OTFo[wo+1, :] # New matrix contains central row of otf (lateral cross-section through the theoretical diameter of the symmetrical otf)
     OTFmax =np.max(np.abs(OTFo))    # Peak value in OTF (presumably central, DC component?)
     OTFtruncate = 0.01   # Scaling factor
-    i = 1   # Iteration counter
-    while (np.abs(OTF1[1,i]) < OTFtruncate * OTFmax): # Needs edit to OTF1[i] to reflect 1D nature of array.
+    i = 0   # Iteration counter
+    while (np.abs(OTF1[i]) < OTFtruncate * OTFmax):# Needs edit to OTF1[i] to reflect 1D nature of array.
+  #      print(i)
     #while the absolute value of given point in the central row of OTF is less than the scale factor multiplied by the max value, for each value of i in x:
     # what is the shape of OTF1? surely it is 1D? Yes, confirmed on testing.
         Kotf = wo+1-i #The half width of the FT +1 minus the iteration count: which point in frequency space axis (1/x) are we at?
@@ -52,6 +64,10 @@ def OTFedgeF(OTFo):
 # Done ?
 #Find input/function names confusing, not easy to know what it is expecting/outputting!
 # The name refers to a 2 opt system for SIM developed by Kai et al. Paper link: https://www.osapublishing.org/DirectPDFAccess/C3B732C2-A58C-DD03-9E565EDD53E3B0C2_248609/oe-21-2-2032.pdf?da=1&id=248609&seq=0&mobile=no
+
+
+
+
 def PhaseKai2opt(k2fa, fS1aTnoisy, OTFo):
     w = np.shape(fS1aTnoisy)[0] # The size of the image along one side
     wo = w/2 # Not required
@@ -81,13 +97,12 @@ def PhaseKai2opt(k2fa, fS1aTnoisy, OTFo):
 
     mA = mA / np.sum(fS1aT0 * np.conj(fS1aT0))
 
-    return mA       # Specifically what does this mean in context and what are we aiming for it to be?
-
+    return (-1 * np.abs(mA))       # Specifically what does this mean in context and what are we aiming for it to be?
 
 if __name__ == '__main__':
     filename = '/Users/Ashley/PycharmProjects/SIMple/Data/SLM-SIM_Tetraspeck200_680nm.tif'
 
-# Import data
+    # Import data
     original_data = get_image(filename, 0)
     # FFT the data
     fS1aTnoisy = return_shiffetd_fft(original_data)
@@ -97,6 +112,23 @@ if __name__ == '__main__':
     OTFo = return_shiffetd_fft(psf)
 
     # number of peaks in x and y respectively
-    k2fa = [0.01, 114]
+    k2fa = [0.01, 110]
     # Computes a optimisation variable for the phase. ?closer to 0/1 better?
-    autocorrelation = PhaseKai2opt(k2fa, fS1aTnoisy, OTFo)
+    #autocorrelation = PhaseKai2opt(k2fa, fS1aTnoisy, OTFo)
+    res = minimize(PhaseKai2opt, x0=k2fa, args=(fS1aTnoisy, OTFo), method='Nelder-Mead', tol=0.001)
+    print(res.x)
+    print('The magnitude is', np.sqrt(res.x[0]**2 + res.x[1]**2))
+    print('The angle is', np.arctan2(res.x[1], res.x[0]))
+
+    reconstructed = generate_patter(512, [res.x[0]/1.26, res.x[1]/1.26])
+    reconstructed_f = fft.ifftshift(fft.fft2(reconstructed))
+    position_max = np.argmax(np.abs(reconstructed_f))
+    X, Y = np.unravel_index(position_max, [512, 512])
+    plt.imshow(np.log(np.abs(fS1aTnoisy**2)))
+    plt.plot(Y, X, 'o', fillstyle='none')
+    plt.show()
+
+    plt.plot(np.log(np.abs(fS1aTnoisy[:, 258]))-10)
+    plt.plot(np.log(np.abs(reconstructed_f[:, 258])))
+    plt.show()
+
