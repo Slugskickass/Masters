67e8436cf098beeb40bf9f40d2c099dd18985cf1
RajSeehra <rsseehra1@sheffield.ac.uk>
2020-05-06 13:02:32 +0100

    added a few extra comments and positioned the plt.imshow to the centre of the 3D stack

--- Return1/Raj/Confocal Simulation/Conf_main.py
+++ Return1/Raj/Confocal Simulation/Conf_main.py
@@ -7,14 +7,14 @@ from scipy import signal
 # Made a point in centre of 2D array
 point = np.zeros((51, 51))
 point[25, 25] = 1
-point[20,20] = 1
+point[15,15] = 1
 
 # Made a 3D PSF
 radPSF= sam.radial_PSF(100, 0.05)
 radPSF = np.moveaxis(radPSF, 0, -1)
 
 scan = np.zeros((radPSF.shape[1], radPSF.shape[0], radPSF.shape[2]))
-scan = sam.kernel_filter(radPSF, point)
+scan = np.rot90(sam.kernel_filter(radPSF, point),2)
 
 # Check for flatness in single particle.
 # check = scan[:,:,100]-radPSF[:,:,100]
@@ -27,7 +27,7 @@ pinhole = np.zeros((scan.shape[1], scan.shape[0], scan.shape[2]))
 for i in range(0, scan.shape[2]):
     pinhole[:, :, i] = scan[:, :, i] * spacial_filter
 
-position = 1
+position = 100
 plt.subplot(141)
 plt.imshow(point)
 plt.title("Point")
