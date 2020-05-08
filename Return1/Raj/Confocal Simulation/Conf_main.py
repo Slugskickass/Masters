7cad6ce3d23327127934632d57095a2cc93aff3c
RajSeehra <rsseehra1@sheffield.ac.uk>
2020-05-08 13:13:08 +0100

    Added 3D kernel functionality to the kernel_filter function. allows a 3D sample to be passed across the PSF.

--- Return1/Raj/Confocal Simulation/Conf_main.py
+++ Return1/Raj/Confocal Simulation/Conf_main.py
@@ -5,12 +5,12 @@ import microscPSF as msPSF
 from scipy import signal
 
 # Made a point in centre of 2D array
-point = np.zeros((51, 51))
-point[25, 25] = 1
-point[15,15] = 1
+point = np.zeros((51, 51, 5))
+point[25, 25, 2] = 1
+point[15, 15, 4] = 1
 
 # Made a 3D PSF
-radPSF= sam.radial_PSF(101, 0.05)
+radPSF = sam.radial_PSF(101, 0.05)
 radPSF = np.moveaxis(radPSF, 0, -1)     # The 1st axis was the z-values. Now in order y,x,z.
 
 scan = np.zeros((radPSF.shape[1]+1, radPSF.shape[0]+1, radPSF.shape[2]))
@@ -22,15 +22,15 @@ scan = np.rot90(sam.kernel_filter(radPSF, point), 2)
 # print("minimum: " + str(np.min(check)), "maximum: " + str(np.max(check)))
 
 # Made a gaussian. (Our spacial filter)  NEED SCALE FOR THIS PINHOLE AS IT WILL VASTLY IMPACT QUALITY.
-spacial_filter = sam.Gaussian_Map((scan.shape[1], scan.shape[0]), 0, 0, 0, 0.5, 1)
+spacial_filter = sam.Gaussian_Map((scan.shape[1], scan.shape[0]), 0, 0, 0, 1, 1)
 
 pinhole = np.zeros((scan.shape[1], scan.shape[0], scan.shape[2]))
 for i in range(0, scan.shape[2]):
     pinhole[:, :, i] = np.rot90(sam.kernel_filter(spacial_filter, scan[:, :, i]), 2)
 
-position = 100
+position = 1
 plt.subplot(141)
-plt.imshow(point)
+plt.imshow(point[:,:,2])
 plt.title("Point")
 plt.subplot(142)
 plt.imshow(radPSF[:,:,position])
--- Return1/Raj/Confocal Simulation/Confocal_Samurai.py
+++ Return1/Raj/Confocal Simulation/Confocal_Samurai.py
@@ -99,60 +99,60 @@ def savetiffs(file_name, data):
         images[0].save(file_name, save_all=True, append_images=images[1:])
 
 
-# Kernel filter.
-def kernel_filter(data, matrix):
-    image = data
-    kernel = np.asarray(matrix)
-
-    # Error check in case the matrix has an even number of sides.
-    if kernel.shape[0] % 2 == 0 or kernel.shape[1] % 2 == 0:
-        print("The matrix has an even number of rows and/or columns. Please make them odd and run again.")
-
-    if sum(sum(kernel)) != 1:       # Quick check to ensure the kernel matrix is within parameters.
-        print("Error, this matrix's summation value is not equal to 1. This can change the final image.")
-        print("The program has divided the matrix by the sum total to return it to a value of 1.")
-        print(("This total value is: " + str(sum(sum(kernel)))))
-        kernel = kernel / sum(sum(kernel))
-        print(kernel)
-
-    # Takes the filter size and allows for a rectangular matrix.
-    edge_cover_v = (kernel.shape[0] - 1) // 2
-    edge_cover_h = (kernel.shape[1] - 1) // 2
-
-    # to determine if the file has multiple frames or not.
-    if data.ndim > 2:
-        # adds an edge to allow pixels at the border to be filtered too.
-        bordered_image = np.pad(image, ((edge_cover_v, edge_cover_v), (edge_cover_h, edge_cover_h), (0, 0)))
-        # Our blank canvas below.
-        processed_image = np.zeros((bordered_image.shape[1], bordered_image.shape[0], bordered_image.shape[2]))
-
-        # Iterates the z, x and y positions.
-        for z in range(0, bordered_image.shape[2]):
-            for x in range(edge_cover_h, bordered_image.shape[1] - edge_cover_h):
-                for y in range(edge_cover_v, bordered_image.shape[0] - edge_cover_v):
-                    kernel_region = bordered_image[y - edge_cover_v:y + edge_cover_v + 1,
-                                    x - edge_cover_h:x + edge_cover_h + 1, z]
-                    k = (kernel * kernel_region).sum()
-                    processed_image[y, x, z] = k
-        # Cuts out the image to be akin to the original image size.
-        processed_image = processed_image[edge_cover_v:processed_image.shape[0] - edge_cover_v,
-                          edge_cover_h:processed_image.shape[1] - edge_cover_h, :]
-
-    else: ### There seems to be an error in this somewhere ###
-        # adds an edge to allow pixels at the border to be filtered too.
-        bordered_image = np.pad(image, ((edge_cover_v, edge_cover_v), (edge_cover_h, edge_cover_h)))
-        # Our blank canvas below.
-        processed_image = np.zeros((bordered_image.shape[1], bordered_image.shape[0]))
-
-        # Iterates the x and y positions.
-        for x in range(edge_cover_h, bordered_image.shape[1]-edge_cover_h):
-            for y in range(edge_cover_v, bordered_image.shape[0]-edge_cover_v):
-                kernel_region = bordered_image[y-edge_cover_v:y+edge_cover_v+1, x-edge_cover_h:x+edge_cover_h+1]
-                k = (kernel * kernel_region).sum()
-                processed_image[y, x] = k
-        # Cuts out the image to be akin to the original image size.
-        processed_image = processed_image[edge_cover_v:processed_image.shape[0]-edge_cover_v, edge_cover_h:processed_image.shape[1]-edge_cover_h]
-    return processed_image
+# Kernel filter. # Old version. Keeping as legacy till new one is fully tested.
+# def kernel_filter(data, matrix):
+#     image = data
+#     kernel = np.asarray(matrix)
+#
+#     # Error check in case the matrix has an even number of sides.
+#     if kernel.shape[0] % 2 == 0 or kernel.shape[1] % 2 == 0:
+#         print("The matrix has an even number of rows and/or columns. Please make them odd and run again.")
+#
+#     if sum(sum(kernel)) != 1:       # Quick check to ensure the kernel matrix is within parameters.
+#         print("Error, this matrix's summation value is not equal to 1. This can change the final image.")
+#         print("The program has divided the matrix by the sum total to return it to a value of 1.")
+#         print(("This total value is: " + str(sum(sum(kernel)))))
+#         kernel = kernel / sum(sum(kernel))
+#         print(kernel)
+#
+#     # Takes the filter size and allows for a rectangular matrix.
+#     edge_cover_v = (kernel.shape[0] - 1) // 2
+#     edge_cover_h = (kernel.shape[1] - 1) // 2
+#
+#     # to determine if the file has multiple frames or not.
+#     if data.ndim > 2:
+#         # adds an edge to allow pixels at the border to be filtered too.
+#         bordered_image = np.pad(image, ((edge_cover_v, edge_cover_v), (edge_cover_h, edge_cover_h), (0, 0)))
+#         # Our blank canvas below.
+#         processed_image = np.zeros((bordered_image.shape[1], bordered_image.shape[0], bordered_image.shape[2]))
+#
+#         # Iterates the z, x and y positions.
+#         for z in range(0, bordered_image.shape[2]):
+#             for x in range(edge_cover_h, bordered_image.shape[1] - edge_cover_h):
+#                 for y in range(edge_cover_v, bordered_image.shape[0] - edge_cover_v):
+#                     kernel_region = bordered_image[y - edge_cover_v:y + edge_cover_v + 1,
+#                                     x - edge_cover_h:x + edge_cover_h + 1, z]
+#                     k = (kernel * kernel_region).sum()
+#                     processed_image[y, x, z] = k
+#         # Cuts out the image to be akin to the original image size.
+#         processed_image = processed_image[edge_cover_v:processed_image.shape[0] - edge_cover_v,
+#                           edge_cover_h:processed_image.shape[1] - edge_cover_h, :]
+#
+#     else: ### There seems to be an error in this somewhere ###
+#         # adds an edge to allow pixels at the border to be filtered too.
+#         bordered_image = np.pad(image, ((edge_cover_v, edge_cover_v), (edge_cover_h, edge_cover_h)))
+#         # Our blank canvas below.
+#         processed_image = np.zeros((bordered_image.shape[1], bordered_image.shape[0]))
+#
+#         # Iterates the x and y positions.
+#         for x in range(edge_cover_h, bordered_image.shape[1]-edge_cover_h):
+#             for y in range(edge_cover_v, bordered_image.shape[0]-edge_cover_v):
+#                 kernel_region = bordered_image[y-edge_cover_v:y+edge_cover_v+1, x-edge_cover_h:x+edge_cover_h+1]
+#                 k = (kernel * kernel_region).sum()
+#                 processed_image[y, x] = k
+#         # Cuts out the image to be akin to the original image size.
+#         processed_image = processed_image[edge_cover_v:processed_image.shape[0]-edge_cover_v, edge_cover_h:processed_image.shape[1]-edge_cover_h]
+#     return processed_image
 
 
 def radial_PSF(xy_size, pixel_size=0.05):
@@ -170,3 +170,82 @@ def radial_PSF(xy_size, pixel_size=0.05):
     psf_total = psf_xy1
 
     return psf_total
+
+
+# Can take 2D/3D arrays and kernels and convolute them.
+def kernel_filter(data, matrix):
+    image = data
+    kernel = np.asarray(matrix)
+
+    # Error check in case the matrix has an even number of sides.
+    if kernel.shape[0] % 2 == 0 or kernel.shape[1] % 2 == 0:
+        print("The matrix has an even number of rows and/or columns. Please make them odd and run again.")
+
+    if kernel.sum() != 1:       # Quick check to ensure the kernel matrix is within parameters.
+        print("Error, this matrix's summation value is not equal to 1. This can change the final image.")
+        print("The program has divided the matrix by the sum total to return it to a value of 1.")
+        print(("This total value is: " + str(kernel.sum())))
+        kernel = kernel / kernel.sum()
+        # print(kernel)
+
+    # Takes the filter size and allows for a rectangular matrix.
+    edge_cover_v = (kernel.shape[0] - 1) // 2
+    edge_cover_h = (kernel.shape[1] - 1) // 2
+
+    # to determine if the file has multiple frames or not, runs a 2D kernel along a 3D array.
+    if data.ndim > 2 and kernel.ndim <= 2:
+        print("2D kernel along a 3D array")
+        # adds an edge to allow pixels at the border to be filtered too.
+        bordered_image = np.pad(image, ((edge_cover_v, edge_cover_v), (edge_cover_h, edge_cover_h), (0, 0)))
+        # Our blank canvas below.
+        processed_image = np.zeros((bordered_image.shape[1], bordered_image.shape[0], bordered_image.shape[2]))
+
+        # Iterates the z, x and y positions.
+        for z in range(0, bordered_image.shape[2]):
+            for x in range(edge_cover_h, bordered_image.shape[1] - edge_cover_h):
+                for y in range(edge_cover_v, bordered_image.shape[0] - edge_cover_v):
+                    kernel_region = bordered_image[y - edge_cover_v:y + edge_cover_v + 1,
+                                    x - edge_cover_h:x + edge_cover_h + 1, z]
+                    k = (kernel * kernel_region).sum()
+                    processed_image[y, x, z] = k
+        # Cuts out the image to be akin to the original image size.
+        processed_image = processed_image[edge_cover_v:processed_image.shape[0] - edge_cover_v,
+                          edge_cover_h:processed_image.shape[1] - edge_cover_h, :]
+
+    elif data.ndim > 2 and kernel.ndim > 2:     # Runs a 3D matrix across a 3D array
+        print("A 3D kernel across a 3D array")
+        edge_cover_d = (kernel.shape[2] - 1) // 2
+        # adds an edge to allow pixels at the border to be filtered too.
+        bordered_image = np.pad(image, ((edge_cover_v, edge_cover_v), (edge_cover_h, edge_cover_h), (edge_cover_d, edge_cover_d)))
+        # Our blank canvas below.
+        processed_image = np.zeros((bordered_image.shape[1], bordered_image.shape[0], bordered_image.shape[2]))
+
+        # Iterates the z, x and y positions.
+        for z in range(edge_cover_d, bordered_image.shape[2] - edge_cover_d):
+            for x in range(edge_cover_h, bordered_image.shape[1] - edge_cover_h):
+                for y in range(edge_cover_v, bordered_image.shape[0] - edge_cover_v):
+                    kernel_region = bordered_image[y - edge_cover_v:y + edge_cover_v + 1,
+                                    x - edge_cover_h:x + edge_cover_h + 1, z - edge_cover_d:z + edge_cover_d + 1]
+                    k = (kernel * kernel_region).sum()
+                    processed_image[y, x, z] = k
+
+        # Cuts out the image to be akin to the original image size.
+        processed_image = processed_image[edge_cover_v:processed_image.shape[0] - edge_cover_v,
+                          edge_cover_h:processed_image.shape[1] - edge_cover_h, edge_cover_d:processed_image.shape[2]-edge_cover_d]
+
+    else: ### There seems to be an error in this somewhere ###
+        print("A 2D kernel across a 2D array")
+        # adds an edge to allow pixels at the border to be filtered too.
+        bordered_image = np.pad(image, ((edge_cover_v, edge_cover_v), (edge_cover_h, edge_cover_h)))
+        # Our blank canvas below.
+        processed_image = np.zeros((bordered_image.shape[1], bordered_image.shape[0]))
+
+        # Iterates the x and y positions.
+        for x in range(edge_cover_h, bordered_image.shape[1]-edge_cover_h):
+            for y in range(edge_cover_v, bordered_image.shape[0]-edge_cover_v):
+                kernel_region = bordered_image[y-edge_cover_v:y+edge_cover_v+1, x-edge_cover_h:x+edge_cover_h+1]
+                k = (kernel * kernel_region).sum()
+                processed_image[y, x] = k
+        # Cuts out the image to be akin to the original image size.
+        processed_image = processed_image[edge_cover_v:processed_image.shape[0]-edge_cover_v, edge_cover_h:processed_image.shape[1]-edge_cover_h]
+    return processed_image
